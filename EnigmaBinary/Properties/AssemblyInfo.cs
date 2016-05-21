0e12c5e7d105a9cb203d3de65a5b1187cd4f4dff
Fred Ekstrand <fredekstrand@gmail.com>
2016-05-21 17:33:18 -0400

    Release 32 bit version

--- EnigmaBinary/Properties/AssemblyInfo.cs
+++ EnigmaBinary/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Encryption.Ciphers")]
-[assembly: AssemblyDescription("Enigma Cipher created by Fred Ekstrand")]
+[assembly: AssemblyTitle("Enigma Binary Cipher")]
+[assembly: AssemblyDescription("Enigma Binary Cipher")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Created by: Fred Ekstrand")]
 [assembly: AssemblyProduct("Encryption.Ciphers")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
